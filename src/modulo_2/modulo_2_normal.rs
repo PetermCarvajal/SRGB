ae2bd5a7587ecf64d28bf2b588da51e281b53071
PetermCarvajal <pmcarvajal_77@cue.edu.co>
2026-07-07 21:28:21 -0500

    Commit #20

--- src/modulo_2/modulo_2_normal.rs
+++ src/modulo_2/modulo_2_normal.rs
@@ -76,28 +76,73 @@ pub fn ejercicio84(){
 
 }
 
+// info Regla 2 (Mutabilidad) + Regla 4 (Enteros sin Signo)
+
 pub fn ejercicio85(){
 
+    /*
+    TODO 85. Declara una variable mutable de tipo i8 simulando la temperatura de un congelador (ej. -10). Réstale 5 grados y vuelve a guardarla.
+    */
+
+    let mut temperatura:i8=-10;
+    temperatura=temperatura-5;
 
 }
 
 pub fn ejercicio86(){
 
+    /*
+    TODO 86. Crea un balance bancario mutable usando i32 con un saldo negativo inicial de -500. Súmale 1000 para sacarlo de números rojos.
+    */
+
+    let mut balance:i32=500;
+    balance=balance+1000;
+
 }
 
 pub fn ejercicio87(){
+
+    /*
+    TODO 87. Declara la altitud de un submarino como mut i16 inicializada en 0. Haz que descienda restándole 150 metros en la siguiente línea.
+    */
+
+    let mut altitud:i16=0;
+    altitud=altitud-150;
 
 }
 
 pub fn ejercicio88(){
 
+    /*
+    TODO 88. Usa un mut i64 para la deuda de una corporación multinacional (valor negativo). Simula un pago dividiendo la deuda a la mitad (/= 2).
+    */
+
+    let mut google:i64=-1500000000;
+    google/=2;
+
 }
 
 pub fn ejercicio89(){
 
+    /*
+    TODO 89. Declara un contador de penalizaciones usando mut i8 inicializado en 0. Resta 1 por cada penalización en tres líneas consecutivas (terminando en -3).
+    */
+
+    let mut contador_penalizaciones:i8=0;
+    contador_penalizaciones-=1;
+    contador_penalizaciones-=1;
+    contador_penalizaciones-=1;
+
 }
 
 pub fn ejercicio90(){
+
+    /*
+    TODO 90. Declara una variable mutable límite mut i32 y asígnale el valor de i32::MIN. Intenta sumarle 1 en la siguiente línea.
+    */
+
+    let mut mutable:i32=i32::MIN;
+    mutable=mutable+1;
 
 }
 
